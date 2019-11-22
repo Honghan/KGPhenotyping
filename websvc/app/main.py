210ce93e0d01ac90be755e28faceee9ea73860a7
honghan <honghan.wu@gmail.com>
2019-11-22 13:37:22 +0000

    enable jsonp http get only api calls

--- websvc/app/main.py
+++ websvc/app/main.py
@@ -2,12 +2,13 @@
 # REST service to handle ODBA requests
 #
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Api, Resource, reqparse
 import json
 import subprocess
 import tempfile
 from os import unlink
+import sys
 
 app = Flask(__name__)
 api = Api(app)
@@ -15,32 +16,40 @@ api = Api(app)
 _classpath = "/app/OntologyPhenotyping-0.0.1-SNAPSHOT.jar:/app/lib/*"
 _class = "ac.uk.hdruk.graph.CLIQuery"
 
+
+def wrap_jsonp(callback, r):
+    print('callback : [%s]' % callback, file=sys.stdout)
+    if callback:
+        return app.response_class(
+            response = callback + '(' + r + ')',
+            status=200,
+            mimetype='application/json'
+        )
+    else:
+        return r, 200
+
+
 class StudyList(Resource):
 
     # list existing studies
     def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("callback")
+        message = request.data
+        args = parser.parse_args()
         result = subprocess.run(["java", "-cp", _classpath, _class, "-l"], \
             stdout=subprocess.PIPE, encoding='utf-8')
         if not result.returncode == 0:
              return "Command exited with non-zero return code", 500
-        return result.stdout, 200
+        return wrap_jsonp(args["callback"], result.stdout)
+
 
 class Study(Resource):
-
-    # get an exisitng study
-    def get(self, name):
-        result = subprocess.run(["java", "-cp", _classpath, _class, "-l", name], \
-            stdout=subprocess.PIPE, encoding='utf-8')
-        if result.stdout.find("NOT_FOUND") == 0:
-             return "Study not found", 404
-        if not result.returncode == 0:
-             return "Command exited with non-zero return code", 500
-        return result.stdout, 200
-
     # create/update a study
-    def put(self, name):
+    def get(self, name):
         parser = reqparse.RequestParser()
         parser.add_argument("rules")
+        parser.add_argument("callback")
 
         message = request.data
         args = parser.parse_args()
@@ -59,19 +68,19 @@ class Study(Resource):
              return "Command exited with non-zero return code", 500
 
         output = { "study": name, "result": result.stdout }
-        status = 200
 
-        return output, status
+        return wrap_jsonp(args["callback"], json.dumps(output))
 
 
 class Query(Resource):
 
     # run a query
-    def post(self):
+    def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument("id")
         parser.add_argument("study")
         parser.add_argument("sparql")
+        parser.add_argument("callback");
 
         message = request.data
         args = parser.parse_args()
@@ -94,15 +103,14 @@ class Query(Resource):
              return "Command exited with non-zero return code", 500
 
         output = { "id": args["id"], "result": result.stdout }
-        status = 200
 
-        return output, status
+        return wrap_jsonp(args["callback"], json.dumps(output))
 
-    def get(self):
+    def post(self):
         return '', 405
 
 api.add_resource(Query, "/query")
-api.add_resource(StudyList, "/study")
+api.add_resource(StudyList, "/liststudy")
 api.add_resource(Study, "/study/<string:name>")
 
 if (__name__ == '__main__'):
